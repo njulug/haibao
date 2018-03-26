18eebefe2f51e79a6eba280662e2402b5029458d
yezhudaxue <dns@yezhudaxue.com>
2018-03-26 17:49:38 +0800

    爬虫更新网站。todo:爬虫更新微信

--- haibao.sh
+++
@@ -1,7 +0,0 @@
-
-cd  /Users/macbookair/Documents/WORK/BLOGS/blog_haibao
-hexo clean && hexo g 
-cp ../hb.png ./public/
-rsync -avhP --delete /Users/macbookair/Documents/WORK/BLOGS/blog_haibao/public/* uv1:/home/wwwroot/haibao.njuer.orgs
-hexo s
-
