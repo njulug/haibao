222390a4223273c66df0f93b9f277ececb6e4ef5
yezhudaxue <dns@yezhudaxue.com>
2018-04-26 20:01:03 +0800

    cronjob

---
+++ cronjob_Haibao.sh
@@ -0,0 +1,7 @@
+# 0 1-23/2 * * * /bin/bash /users/macbookair/CODE/Haibao/haibao/cronjob_Haibao.sh
+
+
+cd /Users/macbookair/CODE/Haibao/haibao
+bash ./quick_downloadAndWriteMD.sh
+bash ~/abras.sh
+bash ./makeWebAndPush.sh 
--- makeWebAndPush.sh
+++ makeWebAndPush.sh
@@ -2,7 +2,8 @@
 cd  /Users/macbookair/Documents/WORK/BLOGS/blog_haibao
 #hexo clean 
 hexo g 
-cp ../hb.png ./public/
+#cp ../hb.png ./public/
+rsync -avhP ../hb.png ./public/
 rsync -avhP --delete /Users/macbookair/Documents/WORK/BLOGS/blog_haibao/public/* uv1:/home/wwwroot/haibao.njuer.org
 #hexo s
 
