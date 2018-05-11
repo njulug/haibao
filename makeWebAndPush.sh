6536f842516cc4260ccb79fcf991d981e1d2ab27
yezhudaxue <dns@yezhudaxue.com>
2018-05-11 17:41:30 +0800

    负载均衡，教育网与外网用不同ip解析

--- makeWebAndPush.sh
+++ makeWebAndPush.sh
@@ -5,5 +5,6 @@ hexo g
 #cp ../hb.png ./public/
 rsync -avhP ../hb.png ./public/
 rsync -avhP --delete /Users/macbookair/Documents/WORK/BLOGS/blog_haibao/public/* uv1:/home/wwwroot/haibao.njuer.org
+rsync -avhP --delete /Users/macbookair/Documents/WORK/BLOGS/blog_haibao/public/* se:/home/wwwroot/haibao.njuer.org
 #hexo s
 
